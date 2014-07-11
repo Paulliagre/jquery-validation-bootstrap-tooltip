8a7662ac6a4bb1c06c3c97d62b854a252b645578
Paulliagre <Paulliagre@gmail.com>
2014-07-11 17:25:27 +0200

    Update jquery-validate.bootstrap-tooltip.js

--- jquery-validate.bootstrap-tooltip.js
+++ jquery-validate.bootstrap-tooltip.js
@@ -6,37 +6,57 @@
  * Copyright 2014 Tom Hiller
  * Released under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * add class .has-error on the parent element 
+ * add the case when the input is :hidden
  */
-(function($) {
-	$.extend(true,$.validator, {
-		prototype:{
-			defaultShowErrors: function() {
-				var self=this;
-				$.each( this.successList, function(index, value) {
-					$(value).removeClass(self.settings.errorClass).addClass(self.settings.validClass).tooltip('destroy');
-				});
-				$.each( this.errorList, function(index, value) {
-					$(value.element).removeClass(self.settings.validClass).addClass(self.settings.errorClass).tooltip('destroy').tooltip(self.apply_tooltip_options(value.element,value.message)).tooltip('show'); 			
-				});
-			},
-			
-			apply_tooltip_options: function(element, message){
-				var options={
-					/* Using Twitter Bootstrap Defaults if no settings are given */ 
-					animation: $(element).data('animation')||true,
-					html: $(element).data('html')||false,
-					placement: $(element).data('placement')||'top',
-					selector: $(element).data('animation')||true,
-					title: $(element).attr('title')||message,
-					trigger: $.trim('manual '+($(element).data('trigger')||'')),
-					delay: $(element).data('delay')||0,
-					container: $(element).data('container')||false
-				};
-				if(this.settings.tooltip_options&&this.settings.tooltip_options[element.name]){
-					$.extend(options,this.settings.tooltip_options[element.name]);
-				}
-				return options;
-			},
-		}
-	});
-}(jQuery));
+(function ($) {
+    $.extend(true, $.validator, {
+        prototype: {
+            defaultShowErrors: function () {
+                var self = this;
+                $.each(this.successList, function (index, value) {
+                    var $el = $(value);
+                    if ($el.is(":visible"))
+                        $el.removeClass(self.settings.errorClass).addClass(self.settings.validClass).tooltip('destroy');
+                    else
+                        $el.parent().removeClass(self.settings.errorClass).addClass(self.settings.validClass).tooltip('destroy');
+                    
+                    $el.parent().removeClass(self.settings.errorClass);
+                });
+                $.each(this.errorList, function (index, value) {
+                    var $el = $(value.element);
+                    if ($el.is(":visible"))
+                        $el.removeClass(self.settings.validClass).addClass(self.settings.errorClass).tooltip('destroy').tooltip(self.apply_tooltip_options(value.element, value.message)).tooltip('show');
+                    else
+                        $el.parent().removeClass(self.settings.validClass).addClass(self.settings.errorClass).tooltip('destroy').tooltip(self.apply_tooltip_options(value.element, value.message)).tooltip('show');
+                   
+                    $el.parent().addClass(self.settings.errorClass);    // Modif PLI
+                });
+            },
+
+            apply_tooltip_options: function (element, message) {
+                var data = $(element).data();
+                var defaut = {
+                    /* Using Twitter Bootstrap Defaults if no settings are given */
+                    animation: true,
+                    html: false,
+                    placement: 'top',
+                    selector: true,
+                    title: message,
+                    delay: 0,
+                    container: false
+
+                };
+                data.trigger = $.trim('manual ' + (data.trigger || ''));
+
+                var options = $.extend({}, defaut, data);
+
+                if (this.settings.tooltip_options && this.settings.tooltip_options[element.name]) {
+                    $.extend(options, this.settings.tooltip_options[element.name]);
+                }
+                return options;
+            }
+        }
+    });
+} (jQuery));
